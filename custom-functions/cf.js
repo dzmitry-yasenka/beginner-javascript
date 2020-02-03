7abaf4ec7a1e86c23865cf5207ea208cd0aaf9fa
Dzmitry Yasenka <dzmitry.yasenka@gmail.com>
2020-02-03 22:52:30 +0000

    custom functions

---
+++ custom-functions/cf.js
@@ -0,0 +1,28 @@
+// function definition
+function  calculateBill(billAmount, taxRate = 0.13, tipRate = 0.18) {
+    console.log("Running calculateBill");
+    var totalCost = billAmount + billAmount * taxRate + billAmount * tipRate;
+    return totalCost;
+}
+
+const billAmount = 100;
+const billTotal1 = calculateBill(100, 0.3, 0.15);
+console.log(billTotal1);
+const billTotal2 = calculateBill(100);
+console.log(billTotal2);
+
+var name = "Doctor";
+
+function sayHiTo(name) {
+    return `Hello to ${name}!`;
+}
+
+function docerize(name) {
+    return `Dr. ${name}`;
+}
+
+function yell(message) {
+    return message.toUpperCase();
+}
+
+console.log(yell(sayHiTo(docerize(name))));
