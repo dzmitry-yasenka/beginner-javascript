416650575c523382b70dbbec792989df7703581b
Dzmitry Yasenka <dzmitry.yasenka@gmail.com>
2020-02-04 22:50:59 +0000

    debugging initial commit

---
+++ exercises/debugging-start.js
@@ -0,0 +1,9 @@
+const people = [
+    { name: "Wes", cool: true, country: "Canada"},
+    { name: "Scott", cool: true, country: "Merica"},
+    { name: "Snickers", cool: false, country: "Dob country"},
+];
+
+people.forEach((person, index) => {
+    console.log(person.name);;
+});
